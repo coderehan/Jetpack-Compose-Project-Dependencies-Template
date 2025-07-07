bb3c6c079fbbd6e9874cd84bb9d97cfc0b3c6ed6
Mohammed_Rehan <mohammed.rehan@vida.id>
2025-07-07 18:48:59 +0530

    Comments added

--- app/src/main/java/com/rehan/jetpackcomposealldependencies/MainActivity.kt
+++ app/src/main/java/com/rehan/jetpackcomposealldependencies/MainActivity.kt
@@ -13,6 +13,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.rehan.jetpackcomposealldependencies.ui.theme.JetpackComposeAllDependenciesTheme
 
+// This is MainActivity
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
